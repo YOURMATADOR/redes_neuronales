a5039c67ff972a422148c8acb25c1b72c19157de
Eduardo Avila (TUMATADOR) <jesus.amancilla@alumnos.udg.mx>
2019-03-29 13:12:36 -0600

    perceptron redes neuronales :metal:

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
+// * agrega como 2 parametro la taza de aprendisaje (.01 por defecto) y como tercero la iteracion (10 por defecto)
 const p = new Perceptron(x[0].length);
 
 p.fit(x,y);
+
+console.log('Informacion procesada',x)
+console.log('0 = Iris-setosa1 = Iris-versicolor',y)
 
 let d = document.createElement('div');
 
